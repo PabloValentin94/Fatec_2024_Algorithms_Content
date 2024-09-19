f710b1666f2b5aded771df84198a112cfeff9832
PABLO VALENTIN <pablo.valentin@etec.sp.gov.br>
2024-09-19 16:27:25 -0300

    Atualizando.

---
+++ JavaScript/Aulas/2024-09-19/Fila.js
@@ -0,0 +1,19 @@
+// Disciplina: Estrutura de Dados.
+// Professor: Tiago Antônio da Silva.
+// Descrição: exemplificando o funcionamento de um fila.
+// Autor(a): Pablo Valentin.
+// Data atual: 19/09/2024
+
+// Variáveis
+
+var array = [];
+
+// Código:
+
+array.push(1,2);
+
+const primeira_pessoa = array.shift();
+
+console.table(array);
+
+console.log(primeira_pessoa);
---
+++ JavaScript/Aulas/2024-09-19/Funcoes_Arrays.js
@@ -0,0 +1,38 @@
+// Disciplina: Estrutura de Dados.
+// Professor: Tiago Antônio da Silva.
+// Descrição: funções nativas de arrays em JavaScript.
+// Autor(a): Pablo Valentin.
+// Data atual: 19/09/2024
+
+// Variáveis:
+
+var array = [1,2,3,4];
+
+// Funções:
+
+function Show_Array()
+{
+
+    console.log(array);
+
+}
+
+// Código:
+
+Show_Array();
+
+array.push(10,14,20);
+
+Show_Array();
+
+array.pop();
+
+Show_Array();
+
+array.shift();
+
+Show_Array();
+
+array.unshift(23,12);
+
+Show_Array();
